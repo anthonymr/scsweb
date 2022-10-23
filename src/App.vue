b2cbff2392b4fc7168bd7a80ab0ef06f2212f897
Anthony Martin <anthonymartin20@gmail.com>
2022-10-23 13:33:21 -0400

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <main id="app">
+    <!--<nav>
+      <router-link to="/">Home</router-link>
+    </nav>-->
+    <router-view />
+  </main>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+:root{
+  --black: rgb(32, 32, 32);
+  --green: green;
+  --white: white;
+  --gray: gray;
+  --border: lightgray;
+  --contrast: orange;
+
+  --max-width: 1440px;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+html,
+body,
+main {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,20 @@
+<template> 
+    <header>
+        <div class="scs-header"></div>
+    </header>
+</template>
+
+<style scoped>
+    header {
+        width: 100%;
+        height: 100px;
+        background-color: var(--black);
+        display: flex;
+        justify-content: center;
+    }
+
+    .scs-header{
+        width: var(--max-width);
+        height: 100%;
+    }
+</style>
